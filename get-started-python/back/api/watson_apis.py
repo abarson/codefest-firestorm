da7eb6b1731aaa9049bf93496c38fecdae41af0a
danielberenberg <djberenb@uvm.edu>
2018-04-07 15:01:03 -0400

    ?/

--- get-started-python/back/api/watson_apis.py
+++ get-started-python/back/api/watson_apis.py
@@ -1,19 +1,27 @@
 from watson_developer_cloud import ToneAnalyzerV3, LanguageTranslatorV2, VisualRecognitionV3
+from watson_developer_cloud import natural_language_understanding_v1 as nlv1
+
+
 import json
+
+
+class NaturalLanguagUnderstanding():
+    def __init__(self):
+        pass
+
 
 class ToneAnalysis():
     def __init__(self):
         self.url = "https://gateway.watsonplatform.net/tone-analyzer/api"
         self.username = "3fffce10-8ba2-437c-a31b-2a73204ac962"
         self.password = "eD4FtUDKYaQE"
-        self.tone_analyzer = ToneAnalyzerV3(
-                username=self.username,
-                password=self.password,
-                version='2017-09-26')
+        self.tone_analyzer = ToneAnalyzerV3(username=self.username,
+                                            password=self.password,
+                                            version='2017-09-26')
 
     def analyze_tone(self, utterance):
         json_utterance = [{'text': utterance}]
-        print(json.dumps(self.tone_analyzer.tone_chat(json_utterance), indent=2))
+        return json.dumps(self.tone_analyzer.tone_chat(json_utterance), indent=2)
 
 
 class Translator():
@@ -23,9 +31,8 @@ class Translator():
         self.url = "https://gateway.watsonplatform.net/language-translator/api"
         self.username = "e01c0489-3c59-4d94-a61b-75335f768528"
         self.password = "5EX02KFSpIOP"
-        self.language_translator = LanguageTranslatorV2(
-                username = self.username,
-                password = self.password)
+        self.language_translator = LanguageTranslatorV2(username=self.username,
+                                                        password=self.password)
 
     def translate(self, text, target_language):
         actual_language = self.languages[target_language]
