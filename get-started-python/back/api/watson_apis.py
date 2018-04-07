5829d1e0c81794db2577b472519a24a476d63d8a
abarson <adam.barson@uvm.edu>
2018-04-07 09:59:44 -0400

    Functional tone analyzer

--- get-started-python/back/api/watson_apis.py
+++ get-started-python/back/api/watson_apis.py
@@ -1,18 +1,25 @@
 from watson_developer_cloud import ToneAnalyzerV3
 import json
 
-url = "https://gateway.watsonplatform.net/tone-analyzer/api"
-username = "3fffce10-8ba2-437c-a31b-2a73204ac962"
-password = "eD4FtUDKYaQE"
+class ToneAnalysis():
+    def __init__(self):
+        self.url = "https://gateway.watsonplatform.net/tone-analyzer/api"
+        self.username = "3fffce10-8ba2-437c-a31b-2a73204ac962"
+        self.password = "eD4FtUDKYaQE"
+        self.tone_analyzer = ToneAnalyzerV3(
+                username=self.username,
+                password=self.password,
+                version='2017-09-26')
 
-tone_analyzer = ToneAnalyzerV3(
-    username=username,
-    password=password,
-    version='2017-09-26')
+    def analyze_tone(self, utterance):
+        json_utterance = [{'text': utterance}]
+        print(json.dumps(self.tone_analyzer.tone_chat(json_utterance), indent=2))
+    
 
-print("\ntone_chat() example 1:\n")
-utterances = [{'text': 'I am very happy.', 'user': 'glenn'},
-              {'text': 'It is a good day.', 'user': 'glenn'}]
-print(json.dumps(tone_analyzer.tone_chat(utterances), indent=2))
+toneCheck = ToneAnalysis()
+
+example_sentence = "Why is A.G. Jeff Sessions asking the Inspector General to investigate potentially massive FISA abuse. Will take forever, has no prosecutorial power and already late with reports on Comey etc. Isn’t the I.G. an Obama guy? Why not use Justice Department lawyers? DISGRACEFUL!"
+
+toneCheck.analyze_tone(example_sentence)
 
 
